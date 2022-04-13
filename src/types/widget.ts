3e44e6d99584201579f41d60f267c9563947ab61
Grant Sander <gksander93@gmail.com>
2022-04-13 10:43:17 -0500

    Added postMessage util and types (#2)
    Co-authored-by: Grant Sander <grant.sander@coinbase.com>

---
+++ src/types/widget.ts
@@ -0,0 +1,31 @@
+import { EventMetadata } from './events';
+
+export type WidgetType = 'buy' | 'checkout';
+
+export type IntegrationType = 'direct' | 'secure_standalone';
+
+export type Experience = 'embedded' | 'popup' | 'new_tab';
+
+export type EmbeddedContentStyles = {
+  target?: string;
+  width?: string;
+  height?: string;
+  position?: string;
+  top?: string;
+};
+
+export type CBPayExperienceOptions<T> = {
+  widgetParameters: T;
+  target?: string;
+  appId: string;
+  host?: string;
+  onReady?: () => void;
+  onExit?: (error?: Error) => void;
+  onSuccess?: () => void;
+  onEvent?: (event: EventMetadata) => void;
+  closeOnExit?: boolean;
+  closeOnSuccess?: boolean;
+  embeddedContentStyles?: EmbeddedContentStyles;
+  experienceLoggedIn?: Experience;
+  experienceLoggedOut?: Experience;
+};
