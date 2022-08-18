5901fc1662acb601879b7bcae438808e89ac558a
Lachlan Tweedie <30222060+lachiet@users.noreply.github.com>
2022-08-17 22:18:21 -0400

    build CBPayInstance

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts', 'src/utils/postMessage.ts'],
+  entry: ['src/index.ts', 'src/utils/postMessage.ts', 'src/utils/CBPayInstance.ts'],
   splitting: true,
   sourcemap: true,
   clean: true,
