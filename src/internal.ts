4f1e5afbbcdd05d5eeb06a581b58ab0ea361c055
Aaron Cordova <theqodesmith@gmail.com>
2022-09-01 13:57:44 -0400

    Updated exports.

---
+++ src/internal.ts
@@ -0,0 +1,5 @@
+export type { CBPayInstanceType } from './utils/CBPayInstance';
+export type { MessageCode, MessageData, PostMessageData, SdkTarget } from './utils/postMessage';
+export { CBPayInstance } from './utils/CBPayInstance';
+export { onBroadcastedPostMessage, getSdkTarget, broadcastPostMessage } from './utils/postMessage';
+export { broadcastEvent } from './utils/events';
