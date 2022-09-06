77ee8e8a0c830b1f8c1844bbeb925e0b393c4a0d
Aaron Cordova <theqodesmith@gmail.com>
2022-09-06 11:57:07 -0400

    Updated exports. (#54)

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts', 'src/utils/postMessage.ts', 'src/utils/CBPayInstance.ts'],
+  entry: ['src/index.ts', 'src/internal.ts'],
   splitting: true,
   sourcemap: true,
   clean: true,
