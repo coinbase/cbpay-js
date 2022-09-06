b7684abd7f5a68e1377ea1102787bc9f6399ae76
Aaron Cordova <theqodesmith@gmail.com>
2022-09-06 11:38:22 -0400

    Updated exports.

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { generateOnRampURL } from './onramp/generateOnRampURL';
 export { initOnRamp } from './onramp/initOnRamp';
 export type { InitOnRampParams } from './onramp/initOnRamp';
 export type { CBPayInstanceType } from './utils/CBPayInstance';
+export type { SupportedBlockchains as Blockchains } from './types/onramp';
