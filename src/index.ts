f4c227f7036d744f4010cdc52757893d9dfdeb07
Zack Winchell <zack.winchell@coinbase.com>
2023-10-11 10:08:33 -0700

    Remove SupportedBlockchains type

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ export { generateOnRampURL } from './onramp/generateOnRampURL';
 export { initOnRamp } from './onramp/initOnRamp';
 export type { InitOnRampParams } from './onramp/initOnRamp';
 export type { CBPayInstanceType } from './utils/CBPayInstance';
-export type { SupportedBlockchains as Blockchains } from './types/onramp';
 
 export type { MessageCode, MessageData, PostMessageData, SdkTarget } from './utils/postMessage';
 export { CBPayInstance } from './utils/CBPayInstance';
